d9ecfaec7215326dc07002b3bebbf1c3bb7b6bf4
ee17btech11034 <asiwalraja@gmail.com>
2026-03-12 23:50:53 +0530

    String in-depth

--- Intro Explaination.js
+++ Intro Explaination.js
@@ -1,52 +1,58 @@
 /*
-JS Data types:
-    --> Officially 2 types of Data types Based on "Data Store in memory and Data Access":
-        --> 1. Primitive Data types (Call by val)
-                --> When we give the val to some func or var then it gives the copy of that val not the reference.                
-                --> Number, String, Boolean, Undefined, Null, Symbol, BigInt
-                    --> id1 = symbol("123") 
-                        id2 = symbol("123") 
-                        Here id1 and id2 will be different bcz Symbol returns unique val.
-                    --> const bigNumber = 1234n; // BigInt literal
-
-
-        --> 2. Non-Primitive Data types (Reference Data types)
-                --> Can provide the reference of memory.
-                --> Arrays, Objects, Functions, etc.
-                    --> typeof (functionName) ==> functions but we call it "Object Functions"
-    
-    --> JavaScript is a dynamically typed language not a Static Typed.
-        --> Variable types are determined at runtime, not at compile time.
-        --> can change the variable type on run time as well
-        --> let a = 5; // a is a number
-        --> a = "Hello"; // now a is a string
-
-
-JS Memory:
-    --> 1. Stack Memory:
-        --> All premetive.
-        --> Stores primitive data types and references to non-primitive data types.
-        --> It is faster to access and manage.
-        --> Memory is automatically managed (LIFO - Last In First Out).
-
-    --> 2. Heap Memory:
-        --> Stores non-primitive data types.
-        --> It is slower to access and manage.
-        --> Memory is manually managed.
+JS String:
+    --> we can use '' or "" or `` to declare
+    --> String interpolation:
+        --> Modern way is using  `` (template literals)
+        --> we can use placeholders ${} to insert variable in string
 
 */
 
 "use strict";
 
-let myName = "Raj"; // Primitive data type (String)
-let nicName = myName; // 
+const myName = "Raj"; // A way to declare a string variable 
+    // --> Behind the scene it is also calling new String("Raj") to create a string object
+const nicName = new String("Raj"); // Another way to declare a string variable  
+    // --> Creating a new instance using "new" keyword
+    // --> Created with help of Constructor "String" of class "String"
 
-console.log(`myName: ${myName}, nicName: ${nicName}`);
-nicName = "Raju"; // Changing nicName does not affect myName
-console.log(`After changing nicName,  myName: ${myName}, nicName: ${nicName}`);
+console.log(myName); // Output: Raj
+console.log(nicName); // Output: Raj
+// Do these in browser console to see the difference
 
 
-let obj1 = { name: "Raj", age: 25 }; // Non-primitive data type (Object)
-let obj2 = obj1; // obj2 holds the reference to the same object in memory
-obj2.age = 26; // Changing obj2's age will affect obj1 as well
-console.log(obj1, obj2)
+console.log(nicName.__proto__); // To access the prototype of string
+console.log(nicName.length); // To get the length property of string
+
+// Prototype Methods of String
+console.log(nicName.toUpperCase()); // To get the uppercase property of string
+console.log(nicName.charAt(1)); // To get the character at index 1 of string
+console.log(nicName.indexOf("a")); // To get the index of the character "A" in the string
+
+
+let otherName = "Rajkumar"
+const substr1 = otherName.substring(0, 3); // To get the substring from index [0, 3)
+const substr2 = otherName.substring(-4, 3); // To get the substring from index [0, 3) => Does not obey negative index
+
+const substr3 = otherName.slice(0, 3); // To get the substring from [0, 3)
+const substr4 = otherName.slice(-4, 3); // To get the substring  from -4 to index 3 => Obeys negative index
+
+console.log(substr1); // Output: Raj
+console.log(substr2); // Output: Raj
+console.log(substr3); // Output: Raj
+console.log(substr4); // Output: "" as 3 is in left side and -4 is in right. 
+
+const substr5 = otherName.slice(3, -4);
+console.log(substr5); // Output: k as [3, -4)
+
+
+
+const str1 = "     Hii    \n" // white spaces
+console.log(str1)
+console.log(str1.trim()) // remove start and end whitespaces and new line chars as well
+
+console.log(otherName.replace("jk", "jK"))// replacing "jk" with "JK" => did not change the actual string
+console.log(otherName.includes("jk")) // To check if the string includes "jk" or not
+
+const str2 = "Hello World Welcome to JavaScript"
+
+console.log(str2.split(" ")); // To split the string into an array of substrings based on the separator " " (space)
