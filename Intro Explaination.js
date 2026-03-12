24387501fcf4ede18a03ce14a0eda9f1e8091d63
ee17btech11034 <asiwalraja@gmail.com>
2026-03-12 10:59:28 +0530

    JS Env Setup on Local Machine

---
+++ Intro Explaination.js
@@ -0,0 +1,40 @@
+/*
+Txt file:
+    -> Text file (.txt) also have simple text which can be random 
+    -> Specific format (.c, .py, .js, etc.) tells that words/syntax or structure written in this has a specific meaning like main, const, let, etc.
+    -> To execute these special files we need Interpreters or Compilers.
+    -> txt file and these files can be used as source files means GIT can track these.
+    -> These compilers/interpreters are just software who has capabilities to understand those keywords and concepts and then execute them.
+
+
+JS:
+    -> JS was in Browser. But we pulled and put it in a software. 
+    -> Browsers use JavaScript engines (like V8 in Chrome, SpiderMonkey in Firefox, JavaScriptCore in Safari) to execute JavaScript.
+    -> JS is a high level, interpreted programming Language (that's why slow). 
+        --> in interpreted languages engine interpret script line by line to machine code (native code) and execute it. In compiled languages, your code is compiled to machine code before it is executed. 
+    -> JIT (Just-In-Time) compilation is a technique used by JavaScript engines to improve performance. 
+        --> It compiles JavaScript code to machine code at runtime, allowing for faster execution compared to traditional interpreted languages.
+        --> JIT is combination of compiler and interpreter,
+        --> Compiler compiles the source code to known as intermediate form bytecode, and passed to interpreter which executes it.
+        --> Every browser has its own Engine but used JIT in modern world. https://medium.com/@balayevkerim/is-javascript-compiled-or-interpreted-language-cb5dbbad9d2
+    
+    -> Node.js, Deno, and Bun are JavaScript runtimes — they allow JavaScript to run outside the browser, on your computer or server.
+    -> JS Runtime use the engine (e.g., V8) and add system-level APIs (file system, network, etc.) so JS can do more than just run in a browser
+
+
+Code Editor:
+    -> VS code, JetBrains Fleet (try this), Sublime text, Atom, etc. are code editors.
+    -> Color code, Linting, etc are some features of code editors..
+
+Node:
+    -> Install nodeJs. cmd `node -v` 
+    -> cmd `node file.js` to run the file.
+
+
+
+
+
+
+
+
+    */
