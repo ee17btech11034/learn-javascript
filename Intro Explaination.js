ee62f872704566e51c5f145d73f78dd47f87ac9c
ee17btech11034 <asiwalraja@gmail.com>
2026-03-12 11:19:28 +0530

    Online JS in GitHub Codespace

--- Intro Explaination.js
+++ Intro Explaination.js
@@ -1,35 +1,21 @@
 /*
-Txt file:
-    -> Text file (.txt) also have simple text which can be random 
-    -> Specific format (.c, .py, .js, etc.) tells that words/syntax or structure written in this has a specific meaning like main, const, let, etc.
-    -> To execute these special files we need Interpreters or Compilers.
-    -> txt file and these files can be used as source files means GIT can track these.
-    -> These compilers/interpreters are just software who has capabilities to understand those keywords and concepts and then execute them.
+GitHub:
+    -> Create a repo. > Code > CodeSpace.
+    -> Codespace is a space or machine where we can write code and run it. It is a virtual machine.
+    -> Inside CodeSpace:
+        -> Click on File > View > Command Pallet > Type "Container" then select "Add Dev ..."
+        -> Then "Show All Configurations" > "Search "nodejs" > Select accordingly > Click on ok. 
+        -> Click on Rebuild option if we are getting. 
 
+        -> When we make changes go to Gihub option we get VS. 
+        -> Click on "+" icon near that filename which we made changes and want to track
+        -> Then add a message and click on "Commit to main" option.
+        -> Click on "Three dots" > "Push" option.
 
-JS:
-    -> JS was in Browser. But we pulled and put it in a software. 
-    -> Browsers use JavaScript engines (like V8 in Chrome, SpiderMonkey in Firefox, JavaScriptCore in Safari) to execute JavaScript.
-    -> JS is a high level, interpreted programming Language (that's why slow). 
-        --> in interpreted languages engine interpret script line by line to machine code (native code) and execute it. In compiled languages, your code is compiled to machine code before it is executed. 
-    -> JIT (Just-In-Time) compilation is a technique used by JavaScript engines to improve performance. 
-        --> It compiles JavaScript code to machine code at runtime, allowing for faster execution compared to traditional interpreted languages.
-        --> JIT is combination of compiler and interpreter,
-        --> Compiler compiles the source code to known as intermediate form bytecode, and passed to interpreter which executes it.
-        --> Every browser has its own Engine but used JIT in modern world. https://medium.com/@balayevkerim/is-javascript-compiled-or-interpreted-language-cb5dbbad9d2
-    
-    -> Node.js, Deno, and Bun are JavaScript runtimes — they allow JavaScript to run outside the browser, on your computer or server.
-    -> JS Runtime use the engine (e.g., V8) and add system-level APIs (file system, network, etc.) so JS can do more than just run in a browser
-
-
-Code Editor:
-    -> VS code, JetBrains Fleet (try this), Sublime text, Atom, etc. are code editors.
-    -> Color code, Linting, etc are some features of code editors..
-
-Node:
-    -> Install nodeJs. cmd `node -v` 
-    -> cmd `node file.js` to run the file.
-
+    -> When work is done. Click on "Codespace" in Github navbar
+    -> In right colum down, we can see a machine running. 
+    -> Click on "three dots" > "Delete". this is free machine from our side. 
+    -> Whenever we will go to codespace then it will restart from that point only.
 
 
 
