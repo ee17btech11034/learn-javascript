748e6e17d5af76a2857cf058d95731cd0f8eae35
ee17btech11034 <asiwalraja@gmail.com>
2026-03-12 15:50:26 +0530

    Ecma Statndards, Strict Mode, Data Types

--- Intro Explaination.js
+++ Intro Explaination.js
@@ -1,77 +1,50 @@
 /*
-Variable Declaration (Keywrds used to declare variables):
-    -> var: (var a = 10)
-        --> can be re-assigned (a = 10)
-        --> can be re-declared (var a = 10) in same of different scope
-        --> Hoisted and initialized with undefined at the start of the scope.
-            --> console.log(a) // undefined
-            --> var a = 10;  -> is valid 
-        --> var is function-scoped or globally scoped; Not block-scoped
-            --> Means if we define var in a function we can not access it outside
-            --> but if we define it in a block then we can access
-            --> Hoisted is done in function scope only.
-    
-    -> let
-        --> can be re-assigned (a = 10) but can not be re-declared (let a = 10) in the same scope
-        --> Hoisted but not initialized (Temporal Dead Zone till val is assigned) so can not access it till then.
-        --> So we can say "Not hoisted" (cannot be used before declaration)
-    
-    -> const
-        --> can not be re-assigned or re-declared
-        --> Hoisted but same in TDZ zone) so "Not hoisted" (cannot be used before declaration)
-        --> Must be initialized at declaration.
+Echma Standards:
+    --> A standard for JavaScript that defines the syntax and semantics of the language. It is developed and maintained by the TC39 committee, which consists of JavaScript developers and experts from around the world. The latest version of the Echma standard is ECMAScript 2021 (ES12), which was released in June 2021.
+    --> like user wll give input and how for loop will work and what will be the output. 
+    --> MDN Documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript
+    --> Official Doc: https://tc39.es/ecma262/
+    --> ECMA just write standards not JS. Engines will have to support those standards.
 
-https://stackoverflow.com/questions/33198849/what-is-the-temporal-dead-zone
 
-Print way:
-    -> We can console log multiple variables in a single console using table as well 
-        --> console.table([a, b, c])
+Strict mode:
+    --> Strict mode is a feature in JavaScript that allows you to write more secure and error-free code. 
+    --> It can be enabled by adding the "use strict" directive at the beginning of a JavaScript file or function. 
+    --> When strict mode is enabled, it changes the behavior of certain JavaScript features, such as variable declarations, function parameters, and error handling. 
+    --> For example, in strict mode, you cannot use undeclared variables, and it will throw an error if you try to do so.
+    -- But in modern way it is default enabled.
+
+
+Data Types:
+    --> number :=> range is -2^53 to 2^53, it is used to represent both integers and floating-point numbers. 
+            --> It also includes special values like NaN (Not a Number) and Infinity.
+    --> bigint :=> It is a new data type introduced in ECMAScript 2020 that allows you to represent integers larger than the maximum safe integer (2^53 - 1) that can be represented by the Number data type.
+            --> Used in Trading or big platforms like FB.
+    --> String :=> It is used to represent text and is enclosed in single quotes (' '), double quotes (" "), or backticks (` `) for template literals.
+            --> Always prefer "" or ``
+    --> Boolean :=> It represents a logical value that can be either true or false.
+    --> Null :=> It represents the intentional absence of any object value. It is often used to indicate that a variable has no value or that an object property is empty.
+            --> let a = null;
+            --> console.log(`type of null: ${typeof null}`)  ==> Object
+    --> Undefined :=> It represents the absence of a value or the uninitialized state of a variable. When a variable is declared but not assigned a value, it is automatically assigned the value of undefined.
+    --> Symbol :=> It is a new data type introduced in ECMAScript 2015 that represents a unique identifier. 
+            --> Symbols are often used as property keys in objects to avoid naming conflicts.
+    
+    
+    --> Objects :=> It is a complex data type that allows you to store collections of data and more complex entities. 
+            --> Objects can be created using object literals, constructor functions, or classes. 
+            --> They can have properties (key-value pairs) and methods (functions that operate on the object's data).
+    
+    
     */
 
-const a = 5;
-// a = 6; // This will throw an error because 'a' is declared as a constant and cannot be re-assigned.
+"use strict";
+console.log(a)
+var a = 5;
 
-let b = 10;
-b = 15; // This is allowed because 'b' is declared with 'let' and can be re-assigned.
-// let b = 20; // This will throw an error because 'b' cannot be re-declared in the same scope.
+console.log("a: ", a)
+console.log(`type of a: ${typeof a}`)
 
-var c = 20;
-c = 25; // This is allowed because 'c' is declared with 'var' and can be re-assigned.
-var c = 30; // This is allowed because 'c' can be re-declared with 'var' in the same scope.
-
-d = 6; //is also valid but it is not recommended to use undeclared variables as it can lead to unexpected behavior and bugs in the code. It will be treated as a global variable, which can cause issues if there are other variables with the same name in different scopes.
-d = 9; //this approach creates a global variable https://community.appsmith.com/content/guide/variables-javascript-comprehensive-guide-var-let-and-const#:~:text=this%20approach%20creates%20a%20global%20variable 
-
-
-
-let e; // This is valid, but 'e' is declared without an initial value, so it will be undefined until it is assigned a value.
-
-
-
-console.table([a, b, c, d, e]);
-
-
-{
-    console.log("Inside function");
-    var f1 = 30;
-}
-function func(){
-    console.log("Inside function");
-    var f2 = 30;
-}
-
-// console.log(f2); // This will throw an error because 'f' is declared with 'var' inside the function and is not accessible outside of it.
-console.log(f1); // This will log 30 because 'f1' is declared with 'var' and is function-scoped, so it is accessible outside of the block but not outside of the function.
-
-
-
-console.log(d);
-console.log(typeof d);
-function foo() {
-    var variable1, variable2;
-
-    variable1 = 5;
-    variable2 = 6;
-    return variable1 + variable2;
-}
-console.log("foof:", foo());
+ 
+console.log(`type of null: ${typeof null}`) // Object (this is a known quirk in JavaScript, null is considered an object type)
+console.log(`type of undefined: ${typeof undefined}`) // Undefined
