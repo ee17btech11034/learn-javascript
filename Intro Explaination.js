2b8903835d4a2bebffe259ea4b1a330c1572fd59
ee17btech11034 <asiwalraja@gmail.com>
2026-03-14 23:42:24 +0530

    Filter, Map, chaning, reduce

--- Intro Explaination.js
+++ Intro Explaination.js
@@ -1,131 +1,83 @@
 /*
+Filter:
+    --> forEach does not return anything.
+    --> arr.filter(Callback) //callback with return "true/false"
 
 Map:
-    --> same as objects
-    --> uniques keys
-    --> remeber insert key order.
+    --> filter handles with boolean return and does not return modified val
+    --> arr.map(Callback) //calback return updated val
 
-Hight Order Array Loops:
-    --> loops to traveser array
+Chaining:
+    --> arr.map(C1).map().filter().filter().map() //it can be anylong chain
 
-    --> For-of loop:
-            --> for (const element of obj1) {} 
-            --> element is val
-            --> obj1 can be Array, String, Map 
-            --> object is not iterable using forof
-
-    
-    --> For-in loop
-            --> iterates over enumerable string properties
-            --> for (const element in obj1) {}
-            --> element is index
-            --> obj1 can be Array, String but return indexes so not a good option; 
-            --> no output on map
-            --> Best for Objects as return keys
-
-    --> for-each loop
-            --> arrays ke prototype me hi ye function inserted hai. 
-            --> arr.foreach(CallBackFunction)
-                --> arr.foreach( function (item) {} )
-                --> arr.foreach( (item) => {} )
-                --> arr.foreach( item => {} ) //if single argument
-                --> arr.foreach( func1 ) //function reference if func1 is defined somewhere
-            --> can be used in Array, Map, Set
-            --> Str, objects does not have this.
+Reduce:
+    --> Accumulate and return an single number
+    --> arr.reduce( CallBack, initialVal)
+    --> arr.reduce( (accumulate, num) => (accumulate + num), initialVal)//initialVal is 0 generally
+        --> for first run accumulate = initialVal and then it returns the new val
 */
 
 
+//If I want to return some val from for-each then it is impossible
+
 const arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-const str1 = "Raja Asiwal"
 
-for (const element of arr1) {
-    console.log(element);
-    
-}
-
-
-for (const element of str1) { //all str chars
-    console.log(element);
-    
-}
-
-
-//Map
-const map = new Map()
-
-map.set("IN", "INDIA")
-map.set("US", "USA")
-map.set("NP", "NEPAL")
-map.set("IN", "India") //did mutate
-
-for (const element of map) { //element is array [key, val]
-    console.log(element);
-}
-
-//array de-structure
-for (const [key, val] of map) {
-    console.log(key, ": ", val);
-}
-
-const obj1 = {
-    "name1": "Raj",
-    "name2": "Raja",
-    "name3": "Rajesh",
-}
-
-// for (const element of obj1) { //obj1 is not iterable
-//     console.log(element)
-// }
-
-
-
-
-console.log("+++++++++++++++++ For - in Loop++++++++++++++++++")
-// For-in loop
-// can be used to iterate objects
-
-for (const key in arr1) {    //array
-    console.log(arr1[key]); //not a best way
-}
-
-for (const key in str1) {   //return index like 0, 1, 2
-    console.log(key);
-}
-
-// for (const key in map) {   //return index like 0, 1, 2
-//     console.log(key);
-// }
-
-for (const key in obj1) {   //return index like 0, 1, 2
-    console.log(obj1[key]);
-}
-
-
-
-
-
-console.log("+++++++++++++++++ For - each Loop++++++++++++++++++")
-// For - Each Function
-
-arr1.forEach(function (item) {
-    console.log(item);
+let foreachReturn = arr1.forEach((num) => {
+    console.log(num)
+    return "Yes"
 })
 
-map.forEach((key, val) => {
-    console.log(key, ": ", val);    
+let foreachReturn2 = arr1.forEach((num) => ("Yes")) //implicit return
+
+console.log("foreachReturn: ", foreachReturn); // undefined as for-each does not return anything
+
+
+//if we want to do some calculation on keys then we need filter.
+
+
+const newArr = []
+arr1.forEach((num)=>{
+    if (num > 4){
+        newArr.push(num)
+    }
 })
-
-function print(item){
-    console.log(item);
-}
-arr1.forEach(print) //pass only reference, do not execute this
+console.log(newArr) //This can be achieved by Filter
 
 
 
-//for each has multiple values comming
-arr1.forEach((item, index, arr) => { //each val has access to item, index, whole array
-    console.log(item, index, arr);
-})
+console.log("++++++++++++++++++++++++ Filter ++++++++++++++++++++");
+
+let filterReturn = arr1.filter((num) => (num > 4)) //true when num > 4 so number will be returned
+console.log("filterReturn: ", filterReturn); //[ 5, 6, 7, 8, 9, 10 ]
+
+let filterReturn2 = arr1.filter((num) => {num > 4}) //we will need to use "return num > 4" is using {}
+console.log("filterReturn2: ", filterReturn2); //[] -> Empty array
 
 
-//foreach is best when array of objects in development
+
+
+console.log("++++++++++++++++++++++++ Map ++++++++++++++++++++");
+
+const addedArr1 = arr1.map((num) => (num+3))
+const addedArr2 = arr1.map((num) => {return num+3}) //need to use return as opened an new scope
+
+console.log("addedArr1: ", addedArr1);
+
+
+console.log("++++++++++++++++++++++++ Chaining ++++++++++++++++++++");
+const chainingArr = arr1.map((num)=>(num+10)).map((num)=>(num*2))
+const chainingArr2 = arr1.map((num)=>(num+10))
+                        .map((num)=>(num*2))
+                        .filter((num)=>(num>25))
+
+console.log("chainingArr: ", chainingArr);
+console.log("chainingArr2: ", chainingArr2);
+
+
+
+
+
+console.log("++++++++++++++++++++++++ Reduce ++++++++++++++++++++");
+
+const summ = arr1.reduce((accumulate, num) => (accumulate + num), 0)
+console.log("Sum: ", summ)
