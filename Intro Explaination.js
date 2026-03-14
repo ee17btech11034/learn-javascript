6d5f33d1c413f7a3b136c2f667d9001ccb2508c8
ee17btech11034 <asiwalraja@gmail.com>
2026-03-14 11:27:58 +0530

    Functions, Rest Operator

--- Intro Explaination.js
+++ Intro Explaination.js
@@ -1,98 +1,60 @@
 /*
-JS Objects:
-    --> key, val pair
-    --> 2 ways to declare:
-        --> 1. As an literal
-            --> can have multiple instances
-            --> a = {}
-        --> 2. Using Constructor
-            --> When we create using this then That is a "Singleton" object means it will have a single instance.
-            --> Object.create()
+JS Functions:
+    --> A block of code which can be used multiple times just with one reference.
+    --> function greet(); gret is the name
+    --> functions are hoisted with declare with "undefined" params val.
 
-    --> console.log(obj2.dietplan?.morning.food); //we add ? to be on safer side that if "dietplan" not present then undefined not error
-    --> .assign or spread operator
-    --> de-structure of objects
+    --> Rest operator: (...) is REST and SPREAD operator
+        --> function calculatetotalPrice(...itemPrices){
+            --> itemPrices will be in an array.
 
-    --> JSOn is object with keys as strings and val as string or a datatype.
-        --> Random user me API
-        --> Use JSOn formator to format JSON data
 */      
 
 "use strict";
 
-const obj1 = new Object() //using constructor, it is a Singleton
-console.log(obj1); // {} same 
+function greet(){ //return "undefined"
+    console.log("Hello")
+}
+console.log(greet); //reference to function
+greet(); //execute it
 
 
-// nested Objects:
-const obj2 = {
-    "name": "Raj",
-    "favnumber": [1, 2, 4],
-    "dietplan": {
-        "morning": {
-            "food": ["sprouts", "apple"],
-            "exercise": ["pushups", "yog"],
-        },
-        "evening": {
-            "food": ["carrot", "chapati"],
-            "exercise": ["pushups", "pullups"],
-        }
+//num3 is default param
+function add(num1, num2, num3 = num1 * num2){ //num1, num2, num3 are parameters-> when we define a function
+    console.log("num1: ", num1)
+    console.log("num2: ", num2)
+    console.log("num3: ", num3, "\n")
+    return num1 + num2
+}
+
+// let sum = add(2, 5) //2,5 are arguments
+
+let sum = add() //num1, num2 are undefined but num3 is Nan as undefined*undefined
+sum = add(1) //1, undefined, NaN
+sum = add(2, 3) //2, 3, 6 (3*2)
+sum = add(2, 3, 5) //2, 3, 5
+
+// sum = add(1, "a") // for these scenarios we check all params in function and then we do the calculations
+// That's whyc TypeScript Best
+
+function add(num1, num2, num3 = num1 * num2){ 
+    if (num1 === undefined){
+        console.log("num1 is not defined");        
+        return //return "undefined"
     }
+    else if (!num2){
+        console.log("num1 is not defined"); 
+    }//
+    return num1 + num2
 }
 
-console.log(obj2.dietplan.morning);
-console.log(obj2.dietplan.morning.food);
-console.log(obj2.dietplan?.morning.food); //we add ? to be on safer side that if "dietplan" not present then undefined not error
-console.log(obj2.dietplans?.morning.food); //undefined as dietplans not present
 
 
-const obj3 = {
-    1: "a",
-    2: "b"
+//When we do not know how many args user wil provide then we use REST operator
+//function calculatetotalPrice(val1, val2, ...itemPrices){ //left to right
+function calculatetotalPrice(...itemPrices){
+    console.log(itemPrices)
 }
 
-const obj4 = {
-    3: "c",
-    4: "a",
-    // 1: "b" //aisa hai to wo update kr dega
-}
-
-const obj5 = {obj3, obj4} //{ obj3: { '1': 'a', '2': '2' }, obj4: { '3': 'a', '4': '2' } }
-console.log(obj5);
-
-const obj6 = Object.assign(obj3, obj4) // target is obj3
-console.log("target obj3: ", obj3) //{ '1': 'a', '2': 'b', '3': 'c', '4': 'a' } as obj3 modified
-console.log("obj6: ", obj6)
-
-const obj7 = Object.assign({}, obj3, obj4) // we prefer to use {} as target 
-console.log("target {}: ", obj3)
-console.log("obj7: ", obj7)
-
-
-const obj8 = {...obj3, ...obj4}
-console.log("spread: ", obj8);
-
-console.log(Object.keys(obj8)); //array of keys
-console.log(Object.values(obj8)); //array of values
-console.log(Object.entries(obj8)); //[[key, val], [key, val]]   array of array key values
-
-console.log(obj8.hasOwnProperty('1')); //takes key and return boolean
-
-
-
-// Object De-structure
-const obj9 = {
-    "users_full_name": "Raj",
-    "age": "23",
-}
-
-console.log(obj9.users_full_name); //everytime i will have to write this long text
-
-//de-structure it
-const {users_full_name} = obj9
-console.log(users_full_name);
-
-//use short name
-
-const {users_full_name:userName} = obj9 //************************** */
-console.log(userName); //i can use nic name to call it
+calculatetotalPrice(1, 2, 3)
+calculatetotalPrice(1, 2, 3, 4, 5)
