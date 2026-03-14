d4df9cd297d93121b77b2bd7b1bced9c887d91f9
ee17btech11034 <asiwalraja@gmail.com>
2026-03-14 12:43:23 +0530

    Local, Global Scope, Function define using Name, Reference

--- Intro Explaination.js
+++ Intro Explaination.js
@@ -1,60 +1,48 @@
 /*
-JS Functions:
-    --> A block of code which can be used multiple times just with one reference.
-    --> function greet(); gret is the name
-    --> functions are hoisted with declare with "undefined" params val.
+JS Scope:
+    --> Global Scope:
+        --> Main function's scope or scope of script. 
+        --> gobal scope of NodeJS env and browser env is different. (Read more about it)
+        --> whatever val we define in this will be available in its child scopes.
 
-    --> Rest operator: (...) is REST and SPREAD operator
-        --> function calculatetotalPrice(...itemPrices){
-            --> itemPrices will be in an array.
+    --> Local Scope:
+        --> Local scope is binded by {} in function , if-else, for loop etc
+            --> a. Block Scope:
+                    --> Scope inside a {}
+                    --> Like if-else, for, etc.
+            -->b. function Scope:
+                    --> sScope inside a function {}.
+                    --> function name(){}
+                    --> function scope is a special type of block scope.
+            -> only diff between Block Scope and Function scope is Hoisting of var. 
+                --> var is hoisted outer scope (like global) if defined in block scope
+                --> var is hoisted to function scope if defined in function
+    
 
 */      
 
 "use strict";
 
-function greet(){ //return "undefined"
-    console.log("Hello")
-}
-console.log(greet); //reference to function
-greet(); //execute it
+//we can write code with let or const and the check these. 
 
 
-//num3 is default param
-function add(num1, num2, num3 = num1 * num2){ //num1, num2, num3 are parameters-> when we define a function
-    console.log("num1: ", num1)
-    console.log("num2: ", num2)
-    console.log("num3: ", num3, "\n")
-    return num1 + num2
+
+// Functions:
+
+
+// using function name
+console.log(add1(3)); //4 as functions with Names are hoisted 
+function add1(num){ //hoisted declaration     "function add1(num(undefined));"
+    return num+1
 }
 
-// let sum = add(2, 5) //2,5 are arguments
+console.log(add1(3));
 
-let sum = add() //num1, num2 are undefined but num3 is Nan as undefined*undefined
-sum = add(1) //1, undefined, NaN
-sum = add(2, 3) //2, 3, 6 (3*2)
-sum = add(2, 3, 5) //2, 3, 5
 
-// sum = add(1, "a") // for these scenarios we check all params in function and then we do the calculations
-// That's whyc TypeScript Best
-
-function add(num1, num2, num3 = num1 * num2){ 
-    if (num1 === undefined){
-        console.log("num1 is not defined");        
-        return //return "undefined"
-    }
-    else if (!num2){
-        console.log("num1 is not defined"); 
-    }//
-    return num1 + num2
+//using reference and store that in variable
+//In JS, variables are so powerful that they can hold any val.
+// console.log(add2(3)); //can not access add2 before initialization
+const add2 = function(num){ //function is hoisted but const are in Temporal Dead Zone, will throw error
+    return num+1
 }
-
-
-
-//When we do not know how many args user wil provide then we use REST operator
-//function calculatetotalPrice(val1, val2, ...itemPrices){ //left to right
-function calculatetotalPrice(...itemPrices){
-    console.log(itemPrices)
-}
-
-calculatetotalPrice(1, 2, 3)
-calculatetotalPrice(1, 2, 3, 4, 5)
+console.log(add2(6));
