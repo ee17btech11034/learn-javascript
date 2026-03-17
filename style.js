ddb027f5299e8e1a8658673b08f911725b6feef6
ee17btech11034 <asiwalraja@gmail.com>
2026-03-17 18:41:28 +0530

    lexical scop, closures, IIFE to block the access

--- style.js
+++ style.js
@@ -1,152 +1,113 @@
 /*
-# Getter:
-    --> When we do not want to give access of any property to user.
-    --> Bydefault class creates getter and setter for each property by their name
-    --> if we define getter then we will have to define setter as well
-    --> setter gets the val from constructor as well
-    --> getter always return something
-    --> console.log(user2.uname);  --> triggers getter
-# Setter
-    --> to set the val
-    --> user2.uname = "Rajesh"; // ← This triggers the setter               **********************************
+# Lexical Scoping:
+        --> https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Closures#lexical_scoping
+        --> If we define some variables and an inner function in a function
+            --> then inner function ca access this variable
+        --> a is accesible in main and inner but not outside. This scoping is called Lexical Scoping.
+        --> Reason: inner() execution is done so it clean the memory for that Execution context *********************
 
-## .length property is best example of getter and setter as it is just a property not a function but how does it calculate the length
+# Closures:
+        --> https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Closures
 
-# Important:
-    --> User1 class
-        --> When we create a new instance of class User.
-            --> constructor will be called and it will try to set the values
-            --> But setter with property names also get invoked. 
-                --> It also tries to set the values.                           setter is uname()
-                --> setter has         this.uname    in it.
-                --> this.uname is also refering to setter because of same name. 
-                --> think "uname" is a function of object, How do we access it? Using "this.uname"
-                --> uname as approperty constructor me set ho rha hai. so this.uname will first refer to class methods/properties, etc.
-        
-        --> Here we will get "Error: RangeError: Maximum call stack size exceeded " error with both getter and setter.
+        --> we return reference of inner function to main function.
+        --> when we return  that mean it is bringing            "its lexical scope not execution context"    *********
+        --> // inner function run hua with main2 ke context ke sath , outside main.
+        --> When we want to hide some details then we define them in main and return inner function as inner function can have only output
+        --> Useful in Abstraction, Encapsulation
 
-    --> jinke bhi setter/getter bana rhe hai unko name thoda alag dedo ya fir uname ko change kr do.
+        --> check the index file for real life example
+
+
+# Using IIFE to block access for variables:
+        --> IIFE runs right away when defined.
+        --> if we can return an object and store that then I can access IIFE's properties.
+        --> i can manage return object
 */
 
 
+// Lexical Scoping
 
-/*
-class User{
-    constructor(uname, email, password){
-        this.uname = uname
-        this.email = email
-        this.password = password
+function main(){
+    let a = 5
+    console.log("a inside main: ", a);
+
+    function inner(){
+        let b = 8
+        console.log("a inside inner: ", a);
     }
-    get uname(){ //getter is defined for uname
-        return this.uname.toUpperCaase()
-    }
-    set uname(val){ // setter for uname
-        this.uname = val
-    }
-}
-//whaever properties we set class provides getter and setter
 
-const user1 = new User("Raj", 'raj@email.in', '124421') //RangeError: Maximum call stack size exceeded  ********
-/*
-    Error: RangeError: Maximum call stack size exceeded  
-        
-            this.uname inside setter referes to setter itself. 
-            a  method calling itself is an infonite loop
-* /    
-*/
-
-
-
-
-
-// to fix this we need to use different names
-
-class User2{
-    constructor(uname, email, password){
-        this._uname = uname //jinke bhi setter/getter bana rhe hai unko name thoda alag dedo ya fir uname ko change kr do.
-        this.email = email
-        this.password = password
-    }
-    get uname(){ //getter is defined for uname
-        return this._uname.toUpperCase()
-    }
-    set uname(val){ // setter for uname
-        console.log("val in setter: ", val);
-        console.log("this._uname in setter: ", this._uname);
-        this._uname = val
-    }
+    inner() // execution is done so it clean the memory 
+    // console.log("binside main: ", b); // b is not defined -> so can not access
 }
 
-const user2 = new User2("Raj", 'raj@email.in', '124421')
-console.log(user2._uname); //"Raj"
-// console.log(user2.uname()); //user2.uname is not a function
-console.log(user2.uname); //RAJ  --> getters are calles this way ************************************
+main()
+// console.log("a outside main: ", a);  // a is not defined
 
-// We can not invoke setter directly.
-user2.uname = "Rajesh"; // ← This triggers the setter               **********************************
-
-console.log(user2.uname); //RAJESH
+// a is accesible in main and inner but not outside. This scoping is called Lexical Scoping.
 
 
 
 
 
+//Closures
+function main2(){
+    let a = 5
+    console.log("a inside main2: ", a);
+    function inner(){
+        let b = 8
+        console.log("a inside inner in main2: ", a);
+        console.log("b inside inner in main2: ", a);
+    }
+
+    return inner; //here we are returning reference
+}
+
+
+const inner_ref = main2() // store the reference of inner function
+console.log(inner_ref); //function
+console.log(inner_ref()); // inner function run hua with main2 ke context ke sath
+
+
+
+//other way to write it
+
+function main3(a){
+    let b = 5
+
+
+
+    return function(){   // return reference in same line
+        console.log("inner");
+    }
+}
 
 
 
 
 
+// IIFE to block the acces -> behaves like private methods variables
 
+const clothStore = (function(){
+    const ownerName = "Raj";
+    const salesLog = [];
+    const demandLog = [];
 
+    function userBoughtLogs(name, price){ // yaha function keyword dena padega
+        salesLog.push(`${name} paid the amount of Rs ${price}`)
+        return `Thanks ${name} for buying from us.`
+    }
+    function showownerName(){
+        console.log("Owner is: ", ownerName)
+    }
 
-
-
-
-
-
-// Using function way
-function User3(email, password){
-    this._email = email
-    this._password = password
-    Object.defineProperty(this, "email", {
-        get: function(){
-            return this._email.toUpperCase()
-        },
-        set: function(val){
-            this._email = val
+    return {
+        salesHistory: userBoughtLogs,
+        demandLog,
+        mostdemandItem(){
+            // we can write this way as well
         }
-    })
-}
+    }
+})();
 
-//doing the same what class code os doing
-
-const user3 = new User3("rajaraja@email", "1928")
-console.log(user3.email); //getter --> RAJARAJA@EMAIL
-user3.email = "rajraje@email" // setter --> set the val
-console.log(user3.email); //RAJRAJE@EMAIL
-
-
-
-
-
-// We can create Object based
-
-const object1 = {
-    _email: "abc@email",
-    _password: "123456789",
-
-    get email(){
-        return this._email
-    },
-    set email(val){
-        this._email = val
-    } 
-}
-
-// benefit is -> we can use factory function like constructor functions
-// const user4 = Object.create() // create an obje which is refring to null as parent or reference
-const user4 = Object.create(object1) //create an object which is refering from object1
-
-console.log(user4); //{} because console.log       only shows own properties, not the inherited ones. ***************
-console.log(user4.__proto__) // { _email: 'abc@email', _password: '123456789', email: [Getter/Setter] }
-console.log(user4.email); //abc@email
+// Here we can acces thing only ion return object using                   "clothStore."
+// even               typeof(clothStore.ownerName) => undefined    --> best way to check any property *************************
